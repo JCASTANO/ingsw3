eb96f190fbd172a12d65a551ce3305e24b260076
Juan Esteban Castano Rincon <estebancastanorincon@gmail.com>
2022-04-05 17:47:42 -0500

    Ajustes para consumir desde front

--- src/main/java/com/uco/myproject/infraestructura/controlador/ControladorPersona.java
+++ src/main/java/com/uco/myproject/infraestructura/controlador/ControladorPersona.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/personas")
 public class ControladorPersona {
