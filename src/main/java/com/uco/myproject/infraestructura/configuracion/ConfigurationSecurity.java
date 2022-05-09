278b62809d394027d94cfb77dc7dd4b65d794dbd
Juan Esteban Castano Rincon <estebancastanorincon@gmail.com>
2022-05-09 13:58:17 -0500

    Open api

--- src/main/java/com/uco/myproject/infraestructura/configuracion/ConfigurationSecurity.java
+++ src/main/java/com/uco/myproject/infraestructura/configuracion/ConfigurationSecurity.java
@@ -15,7 +15,9 @@ public class ConfigurationSecurity {
     public FilterRegistrationBean<FiltroAutenticacion> authenticationFilter(ServicioValidacionToken tokenValidationService){
         FilterRegistrationBean<FiltroAutenticacion> registrationBean = new FilterRegistrationBean<>();
 
-        registrationBean.setFilter(new FiltroAutenticacion(tokenValidationService, new String[]{"/api/login","/api/usuarios"}));
+        registrationBean.setFilter(new FiltroAutenticacion(tokenValidationService, new String[]{"/api/login","/api/usuarios",
+                "/swagger-ui.html","/swagger-ui/index.html","/v3/api-docs/swagger-config","/v3/api-docs",
+                "*.js","*.css","*.png"}));
         registrationBean.addUrlPatterns(URL_PATTERN);
 
         return registrationBean;
--- src/main/java/com/uco/myproject/infraestructura/controlador/ControladorLogin.java
+++ src/main/java/com/uco/myproject/infraestructura/controlador/ControladorLogin.java
@@ -4,11 +4,14 @@ import com.uco.myproject.aplicacion.dto.DtoLogin;
 import com.uco.myproject.aplicacion.dto.DtoRespuesta;
 import com.uco.myproject.aplicacion.servicio.ServicioAplicacionLogin;
 import com.uco.myproject.infraestructura.aspecto.LogExecutionTime;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/login")
+@Tag(name = "Controlador login")
 public class ControladorLogin {
 
     private final ServicioAplicacionLogin servicioAplicacionLogin;
@@ -19,6 +22,7 @@ public class ControladorLogin {
 
     @PostMapping
     @LogExecutionTime
+    @Operation(summary = "Login", description = "Usado para hacer login")
     public DtoRespuesta<String> login(@RequestBody DtoLogin dto) {
         return this.servicioAplicacionLogin.ejecutar(dto);
     }
